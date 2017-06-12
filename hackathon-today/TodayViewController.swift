9a5c8ff7ea65486312d8ed5b504b4d2426da92d8
Artem Novichkov <artem.novichkov@rosberry.com>
2017-06-12 12:17:22 +0600

    Remove storyboard

--- hackathon-today/TodayViewController.swift
+++ hackathon-today/TodayViewController.swift
@@ -10,16 +10,23 @@ import UIKit
 import NotificationCenter
 import TableViewTools
 
+@objc (TodayViewController)
 class TodayViewController: UIViewController {
         
-    @IBOutlet weak var tableView: UITableView!
+    let tableView = UITableView()
     var tableViewManager: TableViewManager!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.tableFooterView = UIView()
+        view.addSubview(tableView)
         tableViewManager = TableViewManager(tableView: tableView)
         extensionContext?.widgetLargestAvailableDisplayMode = .expanded
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
     }
     
     func loadData() {
